98b9e770848271d152cdae5320ce8b09f0585566
wzhoooh <zujnikita867@gmail.com>
2026-05-31 00:05:13 +0300

    заменил скролл альбомов слайдерами, отредачил плеер

--- HomePage.xaml.cs
+++ HomePage.xaml.cs
@@ -4,6 +4,10 @@ namespace WaveTuneNew
 {
     public partial class HomePage : ContentPage
     {
+        private double _albumsScrollX = 0;
+        private double _genresScrollX = 0;
+        private const double ScrollStep = 200;
+
         public HomePage()
         {
             InitializeComponent();
@@ -25,6 +29,30 @@ namespace WaveTuneNew
         private async void OnAboutClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new AboutPage());
+        }
+
+        private async void OnAlbumsScrollLeft(object sender, EventArgs e)
+        {
+            _albumsScrollX = Math.Max(0, _albumsScrollX - ScrollStep);
+            await AlbumsScroll.ScrollToAsync(_albumsScrollX, 0, true);
+        }
+
+        private async void OnAlbumsScrollRight(object sender, EventArgs e)
+        {
+            _albumsScrollX += ScrollStep;
+            await AlbumsScroll.ScrollToAsync(_albumsScrollX, 0, true);
+        }
+
+        private async void OnGenresScrollLeft(object sender, EventArgs e)
+        {
+            _genresScrollX = Math.Max(0, _genresScrollX - ScrollStep);
+            await GenresScroll.ScrollToAsync(_genresScrollX, 0, true);
+        }
+
+        private async void OnGenresScrollRight(object sender, EventArgs e)
+        {
+            _genresScrollX += ScrollStep;
+            await GenresScroll.ScrollToAsync(_genresScrollX, 0, true);
         }
     }
 }
